7250bc59cd17ee86e68ca08009fb56aa5e8c362c
Budi Irawan <deerawan@gmail.com>
2016-05-15 14:58:49 +0700

    add http to phone service and phone class

--- src/app/phone.service.ts
+++ src/app/phone.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class PhoneService {
 
-  constructor() {}
+  constructor(private http: Http) {}
+
+  getPhones(): Observable<Phone[]> {
+    return this.http.get('public/phones/phones.json')
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    if (res.status < 200 || res.status >= 300) {
+      throw new Error('Response Status: ' + res.status);
+    }
+    let body = res.json();
+    return body.data || {};
+  }
+
+  private handleError(error: any) {
+    let errMsg = error.message || 'server error';
+    console.log(errMsg);
+    return Observable.throw(errMsg);
+  }
 
 }
---
+++ src/app/phone.ts
@@ -0,0 +1,8 @@
+export class Phone {
+  id: number;
+  name: string;
+  age: number;
+  imageUrl: string;
+  carrier: string;
+  snippet: string;
+}
