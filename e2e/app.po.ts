50fb9f5240a5736c335d19be7020a5d80ceceb57
Budi Irawan <deerawan@gmail.com>
2016-07-23 19:33:14 +0700

    test

--- e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class Angular2PhonecatPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('angular2-phonecat-app h1')).getText();
-  }
-}
--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export {environment} from './environment';
-export {Angular2PhonecatAppComponent} from './angular2-phonecat.component';
