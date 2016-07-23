c4505f0a65ec6f91990984dba049ae7497e1b071
Budi Irawan <deerawan@gmail.com>
2016-07-23 22:42:47 +0700

    fix dependencies

--- src/app/shared/phone.service.ts
+++ src/app/shared/phone.service.ts
@@ -3,7 +3,7 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { Phone } from './phone';
+import { Phone } from './phone.model';
 
 @Injectable()
 export class PhoneService {
